632d68778224757585a232070b1cf09137b98998
A-Merino <A-Merino>
2025-01-15 21:19:32 -0500

    update 1.0.3

--- src/comps/ClassCard/ClassCard.jsx
+++ src/comps/ClassCard/ClassCard.jsx
@@ -3,6 +3,8 @@ import './ClassCard.css';
 
 function ClassCard(props) {
 
+    const [front, setFront] = useState(true);
+
     let gradeClass = "na-grade";
 
     if (props.data.grade === "A") {
@@ -11,18 +13,38 @@ function ClassCard(props) {
         gradeClass = "b-grade";
     }
   
+    // Flips the class card from grade to description or vice versa
+    const flipCard = (event) => {
+        setFront(!front)
+    }
 
-    return (
-    <>
-        <div className="class-card">
-            <h3 className="card-title">{props.data.class}</h3>
-            <p className="card-grade">Grade: <span className={gradeClass}>{props.data.grade}</span></p>
-            <p className="card-sem">{props.data.semester}</p>
-            <p className="card-prof"><a href={props.data.link}>Professor: {props.data.professor}</a></p>
 
-        </div>
-    </>
-    );
+    if (front) {
+        return (
+        <>
+            <div id={`${props.data.id}-card`} className="class-card" onClick={flipCard}>
+                <h3 className="card-title">{props.data.class}</h3>
+                <p className="card-grade">Grade: <span className={gradeClass}>{props.data.grade}</span></p>
+                <p className="card-sem">{props.data.semester}</p>
+                <p className="card-prof"><a href={props.data.link} target="_blank">Professor: {props.data.professor}</a></p>
+
+            </div>
+        </>
+        );
+
+    } else {
+        return (
+        <>
+            <div id={`${props.data.id}-card`} className="class-card" onClick={flipCard}>
+                <h3 className="card-title">{props.data.class}</h3>
+                <p className="card-desc">{props.data.description}</p>
+
+            </div>
+        </>
+        );
+    }
+
 }
 
 export default ClassCard;
+
