622a04b1ed8f7c2722a75e27faa71f21a1706a6c
A-Merino <alexquiquemerino@gmail.com>
2025-05-13 14:55:14 -0400

    1.2.2

--- src/comps/App/App.jsx
+++ src/comps/App/App.jsx
@@ -7,7 +7,7 @@ import Photography from './../Photography/Photography.jsx';
 import Projects from './../Projects/Projects.jsx';
 import DLP1 from './../Projects/prem/DLP1.jsx';
 import ProjHolder from './../Projects/ProjHolder/ProjHolder.jsx';
-
+import Soon from './../Projects/Soon/Soon.jsx';
 import Settings from './../Settings/Settings.jsx';
 import ChangeLog from './../ChangeLog/ChangeLog.jsx';
 import {Link} from 'react-router';
@@ -26,6 +26,11 @@ function App() {
                 <Route path='/projects' element={<Projects/>}>
                     <Route index element={<ProjHolder/>}/>
                     <Route path='prem-market-value' element={<DLP1/>}/>
+                    <Route path='numerical/program1' element={<Soon/>}/>
+                    <Route path='numerical/program2' element={<Soon/>}/>
+                    <Route path='numerical/program3' element={<Soon/>}/>
+                    <Route path='numerical/program4' element={<Soon/>}/>
+                    
                 </Route>
                 <Route path='/changelog' element={<ChangeLog/>}/>       
             </Routes>
---
+++ src/comps/Projects/Soon/Soon.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './Soon.css'
+
+function Soon() {
+
+    return (
+        <>
+        <h1>Coming Soon</h1>
+        </>
+    )
+}
+
+export default Soon
